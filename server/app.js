8ad23d17592b26b5acb10b071ebdfade797f1a99
uinika <uinika@163.com>
2016-06-15 01:59:41 +0800

    Fix bug

--- server/app.js
+++ server/app.js
@@ -2,5 +2,6 @@ const Koa = require('koa'),
       Static = require('koa-static'),
       App = Koa();
 
-App.use(Static('client'));
+App.use(Static('.'));
 App.listen(5002);
+console.log('HTTP Server listen on port 5002');
