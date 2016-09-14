70311ead4091008345e06467d564437e6aaabf80
xulan <xulan@chinawiserv.com>
2016-09-14 16:39:39 +0800

    All chart debug finished

--- artifact/partials/repository/laws/script.js
+++ artifact/partials/repository/laws/script.js
@@ -18,95 +18,71 @@
     }
   ]);
 
+  laws.service('laws.chart', ['$http', 'URL',
+    function($http, URL) {
+      if (URL) {
+        return $http({
+          method: 'GET',
+          url: URL + '/law/find/findClass',
+          withCredentials: true
+        });
+      } else {
+        console.error('API Not Found in config.js');
+      }
+    }
+  ]);
+
 
   /** Directive */
-  laws.directive('wiservChartLaws', [
-    function() {
+  laws.directive('wiservChartLaws', ['laws.chart',
+    function(lawsChart) {
       return {
         restrict: 'ACE',
         template: "<div id='chartLaws'></div>",
         link: function(scope, element, attrs) {
-          var option = {
-            title: {
-              text: '法条库 汇聚全国法院依法法规数据共 23,332 份，今日新增 3,000 份',
-              x: 'center',
-              y: 'bottom'
-            },
-            tooltip: {
-              trigger: 'item',
-              formatter: "{a} <br/>{b}: {c}条 ({d}%)"
-            },
-            legend: {
-              orient: 'vertical',
-              x: 'left',
-              data: ['社会法', '民法', '商法', '行政法', '刑法', '经济法']
-            },
-            grid: {
-              y:'top'
-            },
-            series: [{
-              name: '法条库',
-              type: 'pie',
-              selectedMode: 'single',
-              radius: [0, '30%'],
-
-              label: {
-                normal: {
-                  position: 'inner'
-                }
+          lawsChart.then(function(response){
+            var data = response.data.body;
+            console.log(data);
+            var option = {
+              title: {
+                text: '法条库 汇聚全国法院依法法规数据共 23,332 份，今日新增 3,000 份',
+                x: 'center',
+                y: 'bottom'
               },
-              labelLine: {
-                normal: {
-                  show: false
-                }
+              tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b}: {c}条 ({d}%)"
               },
-              data: [{
-                value: 635,
-                name: '社会法',
-                selected: true
-              }, {
-                value: 679,
-                name: '民法'
-              }, {
-                value: 1048,
-                name: '商法'
-              }, {
-                value: 958,
-                name: '行政法'
-              }, {
-                value: 1256,
-                name: '刑法'
-              }, {
-                value: 456,
-                name: '经济法'
-              }]
-            }, {
-              name: '司法解释',
-              type: 'pie',
-              radius: ['40%', '55%'],
+              // legend: {
+              //   orient: 'vertical',
+              //   x: 'left',
+              //   data: ['社会法', '民法', '商法', '行政法', '刑法', '经济法']
+              // },
+              grid: {
+                y:'top'
+              },
+              series: [{
+                name: '法条库',
+                type: 'pie',
+                selectedMode: 'single',
+                radius: [0, '30%'],
 
-              data: [{
-                value: 335,
-                name: '社会法'
-              }, {
-                value: 310,
-                name: '民法'
-              }, {
-                value: 234,
-                name: '商法'
-              }, {
-                value: 135,
-                name: '行政法'
-              }, {
-                value: 1048,
-                name: '刑法'
-              }, {
-                value: 251,
-                name: '经济法'
+                label: {
+                  normal: {
+                    position: 'inner'
+                  }
+                },
+                labelLine: {
+                  normal: {
+                    show: false
+                  }
+                },
+                data: data
               }]
-            }]
-          };
-          echarts.init((element.find('#chartLaws'))[0]).setOption(option);
+            };
+            echarts.init((element.find('#chartLaws'))[0]).setOption(option);
+          })
+
         }
       }
     }
---
+++ document/repository/fatiao.md
@@ -0,0 +1,27 @@
+##查询案由顶级分类及该分类下案由的条数
+### /case_brief/find/findClass
+    Type:GET
+#### Parameter:
+    null
+#### Result:
+     name          String         案由分类的名称
+     value         String         该分类案由条数
+----
+##查询法条法律名称及该法律下的法条数
+### /law/find/findClass
+    Type:GET
+#### Parameter:
+    null
+#### Result:
+     name          String         法律名称
+     value         String         法律条款数量
+----
+##查询裁决文书不同案件的类型和该类型的案件数量
+ ### /legal/find/findClass
+     Type:GET
+ #### Parameter:
+     null
+ #### Result:
+      name          String         案件类型名称
+      value         String         该案件类型的案件数
+ ----
