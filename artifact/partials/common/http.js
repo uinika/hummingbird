80dc15776901458991d00291ce851dbd8c0f4abd
uinika <zhengh@chinawiserv.com>
2016-09-20 16:43:21 +0800

    Fix bug

--- artifact/partials/common/http.js
+++ artifact/partials/common/http.js
@@ -3,9 +3,9 @@
   var module = angular.module('common.http', []);
 
   module.constant('URL',
-      'http://localhost:5005'
+      // 'http://localhost:5005'
       // 'http://172.16.1.67:8080/court_decision_system'
-      // 'http://172.16.1.68:8080/court_decision_system'
+      'http://172.16.1.68:8080/court_decision_system'
   );
 
   module.factory('validate', [
