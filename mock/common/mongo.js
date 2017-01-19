0a9abe0dd35b2ed25120b83c385c53df862754f5
uinika <zhengh@chinawiserv.com>
2017-01-20 00:00:32 +0800

    'Deprecated'

--- mock/common/mongo.js
+++
@@ -1,6 +0,0 @@
-const Mongodb = require('mongodb');
-
-/** Mongodb connection */
-exports.mongodb = new Mongodb.Db('autumn', new Mongodb.Server('localhost', 27017), {
-  safe: true
-});
--- mock/login/api.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router(),
-      Util = require('../common/util.js');
-/** Router definition */
-Router.route('/login')
-  .post(function(request, response) {
-    response.json(Util.json('/login/data/login.json'));
-});
-/** Module export */
-module.exports = Router;
